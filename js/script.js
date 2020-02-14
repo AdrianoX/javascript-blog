0258b13dfad21953c463d84b6320d6e24d4a75fb
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-02-14 20:45:26 +0100

    Add new class active (with add)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
   const titleClickHandler = function(event){
-    console.log('Link was clicked!', event);
+    const clickedElement = this;
+    console.log('Link was clicked!');
 
       
-  /* remove class 'active' from all article links */
+  /* [DONE] remove class 'active' from all article links */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -10,15 +11,22 @@
     activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+   
+   clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+   console.log('clickedElement:', clickedElement);
 
-  const activeArticles = document.querySelectorAll('.posts .active');
+   //console.log('clickedElement (with plus): ' + clickedElement);//
+
+  /* [DONE] remove class 'active' from all articles */
+
+  /*const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
+    activeArticle.classList.remove('active'); 
+  } */
+
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
